852b6427861b27e62334430016e4a5d13985eed5
Felismino Chico <felisminochico35@gmail.com>
2024-12-06 16:51:46 +0000

    Novas atualizações

--- javascript/tabuada.js
+++ javascript/tabuada.js
@@ -3,12 +3,10 @@ let botaoDivisao = window.document.getElementsByTagName('button')[0];
 let botaoMultiplicacao = window.document.getElementsByTagName('button')[1];
 let botaoSoma = window.document.getElementsByTagName('button')[2];
 let botaoSubtracao = window.document.getElementsByTagName('button')[3];
-let resultado = window.document.getElementById('resultado');
+let resultadoTabuada = window.document.getElementById('resultado');
 let numeroTabuada = window.document.querySelector('input#numero');
 let inicioContagem = window.document.getElementsByName('inicio')[0];
 let fimContagem = window.document.getElementById('fim');
-let resultadoFinal = 0;
-let c = 0;
 
 //Eventos
 botaoDivisao.addEventListener('click', divisao)
@@ -18,18 +16,25 @@ botaoSubtracao.addEventListener('click', subtracao)
 
 //Funções de eventos
 function divisao(){
-    if(inicioContagem.value < fimContagem.value){
-        resultado.innerHTML = '';
-        for(c = inicioContagem.value; c <= fimContagem.value; c++){
-            resultadoFinal = Number(numeroTabuada.value) / Number(c)
-            resultado.innerHTML += `${numeroTabuada.value} / ${c} = ${resultadoFinal} <br>`;
+    let n = Number(numeroTabuada.value);
+    let i = Number(inicioContagem.value);
+    let f = Number(fimContagem.value);
+    let resultadoFinal = 0;
+
+    if(i < f){
+        resultadoTabuada.innerHTML = '';
+        for(let c = i; c <= f; c++){
+            resultadoFinal = n / c;
+            resultadoTabuada.innerHTML += `${n} / ${c} = ${resultadoFinal.toFixed(2).replace('.', ',')} <br >`;
         }
-    }else if(inicioContagem.value > fimContagem.value){
-        resultado.innerHTML = '';
-        for(c = inicioContagem.value; c >= fimContagem.value; c--){
-            resultadoFinal = Number(numeroTabuada.value) / Number(c)
-            resultado.innerHTML += `${numeroTabuada.value} / ${c} = ${resultadoFinal} <br>`;
+    }else if(i > f){
+        resultadoTabuada.innerHTML = '';
+        for(let c = i; c >= f; c--){
+            resultadoFinal = n / c;
+            resultadoTabuada.innerHTML += `${n} / ${c} = ${resultadoFinal.toFixed(2).replace('.', ',')} <br>`;
         }
+    }else if(i == f){
+        resultadoTabuada.innerHTML = `Os valores dos campos Início da Contagem e Fim da Contagem são iguais. Diferencie-os.`;
     }
     
 }
